f5f280ac07764d846e260bb7cfad5f81504e13aa
Michele Chilanti <chilantim@gmail.com>
2019-11-08 13:08:16 -0600

    Remove the controller from the Debian package and tag images on release (#5)
    * Initial code drop
    
    * Removed controller pull
    
    * Tagging images

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+IMAGE_TAG=$TRAVIS_TAG
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t $DOCKER_ORG/debian-builder .
-docker push $DOCKER_ORG/debian-builder
+docker build -t $DOCKER_ORG/debian-builder:$IMAGE_TAG .
+docker push $DOCKER_ORG/debian-builder:$IMAGE_TAG
